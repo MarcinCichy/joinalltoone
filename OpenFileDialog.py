4607af5caa3d41a964611b5b8bb4810ed5aa4dae
MarcinCichy <marcin-cichy@wp.pl>
2023-09-25 06:37:43 +0200

    Added signals and sockets to the "Select Folder" button - you can now open the file dialog

---
+++ OpenFileDialog.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit, QAction, QFileDialog, QApplication)
+from PyQt5.QtGui import QIcon
+import sys
+from pathlib import Path
+
+
+class OpenFileDialog (QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        # open dialog
+        home_dir = str(Path.home())
+        fname = QFileDialog.getOpenFileName(self, 'Open file', home_dir)
+
+        print(fname)
+        #if file selected, print text contents
+        if fname[0]:
+                data = f.read()
+                print(data)
+                exit()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = OpenFileDialog()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
+
